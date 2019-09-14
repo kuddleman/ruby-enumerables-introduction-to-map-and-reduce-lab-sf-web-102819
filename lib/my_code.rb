bcaaec4b2565c1d00c0ac7865fc17fbebcc16c3a
Donald Lobree <edl1155@gmail.com>
2019-09-14 16:36:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,3 +3,8 @@ def map_to_negativize(array)
   new_array = array.map{|number| number * -1}
   new_array
 end
+
+def map_to_no_change(array)
+  new_array = array.map(|number| number)
+  new_array
+end
